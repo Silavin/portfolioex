2db096fd5c96360d09086b9430cbb7ed0eae33d6
Sebastian Sim <s.sim97@hotmail.com>
2018-10-30 10:57:04 +0800

    Installed Development testing dependencies and created setup and teardown for mock DB

---
+++ server/utils/mockDB.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const mockMongoDB = require("mon");
+
+let mongoServer;
+
+const setUpServer = async () => {
+  mongoServer = mockMongoDB();
+  const mongoURI = await mongoServer.getConnectionString();
+
+  const fakeDBEstablished = () => {
+    //eslint-disable-next-line
+    console.log("The mock database has been setup");
+  };
+  const errorHandler = err => {
+    //eslint-disable-next-line
+    console.log("Error has occured within the database:\n" + err);
+  };
+  await mongoose
+    .connect(
+      mongoURI,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true
+      }
+    )
+    .then(fakeDBEstablished, errorHandler);
+};
+
+const tearDown = () => {
+  mongoose.disconnect();
+  mongoServer.stop();
+};
+
+module.exports = {
+  setUpServer,
+  tearDown
+};
